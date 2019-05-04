9afb6766f15de3a32102c7f17205e9ba994093c0
elijah gaytan <elijahgaytan@utexas.edu>
2019-05-04 14:15:01 -0500

    finished enemy spawn

---
+++ Assets/_Scripts/EnemySpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private int minTimeBetweenSpawn = 4;
+    [SerializeField] private int maxTimeBetweenSpawn = 10;
+    [SerializeField] private GameObject enemyToSpawn;
+    private float nextSpawnTime = 0f;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        int timeBetweenSpawns;
+        if (Time.time > nextSpawnTime)
+        {
+            timeBetweenSpawns = Random.Range(minTimeBetweenSpawn, maxTimeBetweenSpawn);
+            Instantiate(enemyToSpawn, transform.position, Quaternion.identity);
+            nextSpawnTime += timeBetweenSpawns;
+        }
+    }
+}

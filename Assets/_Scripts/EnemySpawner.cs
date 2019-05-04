f57d84ac19bd604d6e8343e11eb281bec0d09cb7
elijah gaytan <elijahgaytan@utexas.edu>
2019-05-04 14:19:46 -0500

    enemy spawn revise

--- Assets/_Scripts/EnemySpawner.cs
+++ Assets/_Scripts/EnemySpawner.cs
@@ -8,17 +8,18 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private int maxTimeBetweenSpawn = 10;
     [SerializeField] private GameObject enemyToSpawn;
     private float nextSpawnTime = 0f;
-    
+    private int timeBetweenSpawns;
 
     // Update is called once per frame
     void Update()
     {
-        int timeBetweenSpawns;
+       
         if (Time.time > nextSpawnTime)
         {
             timeBetweenSpawns = Random.Range(minTimeBetweenSpawn, maxTimeBetweenSpawn);
             Instantiate(enemyToSpawn, transform.position, Quaternion.identity);
             nextSpawnTime += timeBetweenSpawns;
+            //Debug.Log(timeBetweenSpawns);
         }
     }
 }

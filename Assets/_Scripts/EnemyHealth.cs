707a2394e0ba1fb06aed8d015ee78c8a18bd73ed
elijah gaytan <elijahgaytan@utexas.edu>
2019-05-04 14:58:03 -0500

    added explosion, health and damage)

--- Assets/_Scripts/EnemyHealth.cs
+++ Assets/_Scripts/EnemyHealth.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour, ITakeDamage
 {
-    public void OnHit(float damage) {
-        
+    [SerializeField] private float health = 1f;
+    [SerializeField] private GameObject DestroyVFX; 
+    
+    
+    public void OnHit(float damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Instantiate(DestroyVFX, transform.position, Quaternion.identity);
+            Destroy(gameObject, 1.5f);
+            //put death anim here
+        }
     }
     
 }
